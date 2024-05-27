28cf740cb5a99040018e4f71bcc0f89e50b88e94
Holmc555 <118386182+Holmc555@users.noreply.github.com>
2024-05-27 13:37:48 +0300

    Initial commit

---
+++ src/main/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.testng.annotations.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+        assertTrue(true);
+    }
+}
